0b0d63d7567d1acfa1abc42aa790c8fafcc15f4d
Wanderson Sousa Rodrigues <141971045+Wanderson4748@users.noreply.github.com>
2025-01-08 22:26:38 -0300

    Commit Cabecalho e algumas vairaveis

---
+++ advinhacao/adivinhacao.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    // Imprime o cabeçalho do nosso jogo
+    printf("***************************************\n");
+    printf("* Bem vindo ao nosso jogo de adivinhação! *\n");
+    printf("******************************\n");
+    
+    int numero_secreto = 42;
+    int chute;
+
+    printf("Qual é o seu chute? ");
+    scanf("%d", &chute);
+    printf("Seu chute foi %d", chute);
+}
