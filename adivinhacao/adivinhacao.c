eafc55ad3d0458c1bfc5c1f5cfbb85171f58e20c
Wanderson Sousa Rodrigues <141971045+Wanderson4748@users.noreply.github.com>
2025-01-11 14:52:30 -0300

    Commit FOR (LOOP)
    Resolver problema do Loop do codigo com o FOR.

--- adivinhacao/adivinhacao.c
+++ adivinhacao/adivinhacao.c
@@ -9,25 +9,32 @@ int main()
     int numero_secreto = 42;
     int chute;
 
+    //for(int i = 1; i <= 3; i++) {}
+    printf("Tentativa %d de 3\n", i);
     printf("Qual é o seu chute? ");
     scanf("%d", &chute);
     printf("Seu chute foi %d\n", chute);
+    
+    int acertou = (chute == numero_secreto);
+    int maior = (chute > numero_secreto);
+    int menor = (chute < numero_secreto);
 
-    if (chute == numero_secreto)
-    {
+    if(acertou) {
         printf("Parabéns, Você acertou!\n");
+        printf("Você é um bom jogador!, Jogue mais uma vez!\n");
     }
-    else
-    {
+    else {
         printf("Você errou!\n");
-        if (chute > numero_secreto)
+        if (maior)
         {
             printf("Seu chute foi maior que o número secreto!\n");
+            printf("Mas não desanime! Tente novamente!\n");
         }
-        if (chute < numero_secreto)
+        if (menor)
         {
             printf("Seu chute foi menor que o número secreto!\n");
+            printf("Mas nãod desanime! Tente novamente!\n");
         }
-        printf("Mas não desanime! Tente novamente!\n");
+        
     }
 }
