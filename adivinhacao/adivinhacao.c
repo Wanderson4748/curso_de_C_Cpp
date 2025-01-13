56939ff5bec6136f7113342bcc3b067ac1e6da83
Wanderson Sousa Rodrigues <141971045+Wanderson4748@users.noreply.github.com>
2025-01-13 07:54:15 -0300

    commit

--- adivinhacao/adivinhacao.c
+++ adivinhacao/adivinhacao.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <stdio.h>  //
+#include <stdlib.h> //
+#include <time.h>   //
 
 int main()
 {
@@ -9,12 +9,13 @@ int main()
     printf("* Bem vindo ao nosso jogo de adivinhação! *\n");
     printf("******************************\n");
 
-////VARIÁVEIS
+    ////VARIÁVEIS PRINCIPAIS/////
+
     int segundos = time(0);
     srand(segundos);
 
     int numero_grande = rand();
-   
+
     int numero_secreto = numero_grande % 100;
     int chute;
     int tentativas = 1;
@@ -26,83 +27,83 @@ int main()
 
     int numero_de_tentativas;
 
-///////////////////////////////////////////////////
+    // PERGUNTAR QUAL A DIFICULDADE DO JOGO
+
     printf("Qual o nível de dificuldade?\n");
     printf("(1) Fácil, (2) Médio, (3) Dificil \n\n");
     printf("Escolha: ");
     scanf("%d", &nivel);
-    
-    switch (nivel) {
-        case 1:
-            numero_de_tentativas = 20;
-            break;
-        
-        case 2:
-            numero_de_tentativas = 15;
-            break;
-        
-        default:
-            numero_de_tentativas = 6;
-            break;
-    }
 
-    if(nivel == 1) {
+    // Verificar o nível e atribui a variavel "numero_de_tentativas"
+
+    switch (nivel)
+    {
+    case 1:
         numero_de_tentativas = 20;
-    }
+        break;
 
-    else if (nivel == 2) {
+    case 2:
         numero_de_tentativas = 15;
-    }
+        break;
 
-    else {
+    default:
         numero_de_tentativas = 6;
+        break;
     }
 
-//Receber o chute do usuario e imprimir o numero de tentativas
-    for (int i = 1; i <= numero_de_tentativas; i++) {
+    //
+    for (int i = 1; i <= numero_de_tentativas; i++)
+    {
 
         printf("Tentativa %d\n", tentativas);
         printf("Qual é o seu chute? ");
-        
+
         scanf("%d", &chute);
         printf("Seu chute foi %d\n", chute);
 
-// Verifica se o chute é negativo
-        if (chute < 0) {
+        // Verifica se o chute é negativo
+        if (chute < 0)
+        {
             printf("Você não pode chutar números negativos!\n");
-            
+
             continue;
         }
-        
+
         int maior = (chute > numero_secreto);
         int menor = (chute < numero_secreto);
 
-        if(acertou) {
+        if (acertou)
+        {
             break;
-            }
+        }
 
-            else if (maior) {
-                printf("Seu chute foi maior que o número secreto!\n");
-                printf("Mas não desanime! Tente novamente!\n");
-            }
-            else if (menor) {
-                printf("Seu chute foi menor que o número secreto!\n");
-                printf("Mas não desanime! Tente novamente!\n");
-            }
-            tentativas++;
-            double pontosperdidos = abs(chute - numero_secreto) / (double)2;
+        else if (maior)
+        {
+            printf("Seu chute foi maior que o número secreto!\n");
+        }
 
-            pontos = pontos - pontosperdidos;
+        else if (menor)
+        {
+            printf("Seu chute foi menor que o número secreto!\n");
         }
-        
-        printf("Fim de jogo!\n");
-        
-        if(acertou) {
-            printf("Você ganhou!\n");
-            printf("Você acertou em %d tentativas!\n", tentativas);
-            printf("Total de pontos: %.1f\n", pontos);
-        }
-        else {
-            printf("Você perdeu! Tente de novo!\n");
-        }
+
+        tentativas++;
+
+        double pontosperdidos = abs(chute - numero_secreto) / (double)2;
+
+        pontos = pontos - pontosperdidos;
+    }
+
+    printf("Fim de jogo!\n");
+
+    if (acertou)
+    {
+        printf("Você ganhou!\n");
+        printf("Você acertou em %d tentativas!\n", tentativas);
+        printf("Total de pontos: %.1f\n", pontos);
+    }
+    else
+    {
+        printf("Você perdeu! Tente de novo!\n");
+    }
 }
