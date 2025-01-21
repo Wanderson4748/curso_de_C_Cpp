a1abf8b1905b69548059d397da7b16c5f2b3817a
Wanderson Sousa Rodrigues <141971045+Wanderson4748@users.noreply.github.com>
2025-01-21 08:51:38 -0300

    commit

---
+++ jogo_forca/anotacoes.c
@@ -0,0 +1,33 @@
+/* #include <stdio.h>
+
+int main()
+{
+    char letra1 = 'M';
+    char letra2 = 'E';
+    char letra3 = 'L';
+    char letra4 = 'A';
+    char letra5 = 'N';
+    char letra6 = 'C';
+    char letra7 = 'I';
+    char letra8 = 'A';
+
+    printf("%c%c%c%c%c%c%c%c", letra1, letra2, letra3, letra4, letra5, letra6, letra7, letra8);
+}
+*/
+
+/*
+#include <stdio.h>
+
+int main()
+{
+
+    char palavra_secreta[20];
+
+    palavra_secreta[0] = 'P';
+    palavra_secreta[1] = 'A';
+    palavra_secreta[2] = 'O';
+    palavra_secreta[3] = '/0';
+
+    printf("%c%c%c", palavra_secreta[0], palavra_secreta[1], palavra_secreta[2]);
+}
+*/
---
+++ jogo_forca/forca.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char palavra_secreta[20];
+
+    sprintf(palavra_secreta, "MELANCIA E MAMAO");
+
+    int acertou == 0;
+    int enforcou == 0;
+
+    do
+    {
+        char chute;
+        scanf("Qual o seu chute? %c", &chute);
+
+        for (int i = 0; i < strlen(palavra_secreta); i++)
+        {
+            if (palavra_secreta[i] == chute)
+            {
+                printf("A posição %d tem essa letra!\n", i);
+            }
+        }
+
+    } while (!acertou && !enforcou);
+}
